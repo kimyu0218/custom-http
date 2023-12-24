1bbb4840179f920f497325ab6c24cdd745f3e420
kimyu0218 <serena35@ewhain.net>
2023-12-24 17:07:25 +0900

    refactor: query parsing logic into seperate file

---
+++ http/utils/parsers/query.parser.ts
@@ -0,0 +1,19 @@
+/**
+ * Parse the query string
+ * @param {string} queryString
+ * @returns {Map<string, string>}
+ */
+export default function parseQuery(queryString: string): Map<string, string> {
+  if (!queryString) {
+    return new Map();
+  }
+  const query: string[] = queryString.split('&');
+  return query.reduce<Map<string, string>>(
+    (acc: Map<string, string>, curr: string) => {
+      const [key, value] = curr.split('=');
+      acc.set(key, value);
+      return acc;
+    },
+    new Map(),
+  );
+}
