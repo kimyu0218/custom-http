0c046f7c7a3de2d8fac5a0f19fe4a0568cfe2e85
kimyu0218 <serena35@ewhain.net>
2023-12-24 22:37:48 +0900

    feat: add parsing logic for the param

---
+++ http/utils/parsers/param.parser.ts
@@ -0,0 +1,25 @@
+/**
+ * Parse the params of the uri
+ * @param {string} uri
+ * @param {string} route
+ * @returns {Map<string, string>}
+ */
+export function parseParam(uri: string, route: string): Map<string, string> {
+  const uriSegments: string[] = uri.split('/').slice(1);
+  const routeSegments: string[] = route.split('/').slice(1);
+
+  // find the indexes of the params
+  const idxs: number[] = uriSegments
+    .map((item: string, idx: number) => (item[0] === ':' ? idx : undefined))
+    .filter((item: number | undefined) => item !== undefined);
+
+  return idxs.reduce<Map<string, string>>(
+    (acc: Map<string, string>, curr: number) => {
+      const key: string = routeSegments[curr].slice(1);
+      const value: string = uriSegments[curr];
+      acc.set(key, value);
+      return acc;
+    },
+    new Map(),
+  );
+}
