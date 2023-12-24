becec944eccda2537c60a9c91a584c46a2232c56
kimyu0218 <serena35@ewhain.net>
2023-12-24 17:05:51 +0900

    fix: change the name and the data type
    - path -> uri
    - Record -> Map

--- http/interfaces.ts
+++ http/interfaces.ts
@@ -2,7 +2,7 @@ import { HttpMethods } from './constants';
 
 export interface ParsedRequestLine {
   method?: HttpMethods;
-  path?: string;
+  uri?: string;
   version?: string;
 }
 
@@ -10,7 +10,7 @@ export interface ParsedRequest {
   method: string;
   path: string;
   version: string;
-  cookies?: Record<string, string>;
-  query?: Record<string, string>;
-  body?: Record<string, string>;
+  cookies?: Map<string, string>;
+  query?: Map<string, string>;
+  body?: any;
 }
