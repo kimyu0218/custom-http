d3629683f099fc728b8ab62d52dea07758ee88a7
kimyu0218 <serena35@ewhain.net>
2023-12-26 22:17:52 +0900

    refactor: delete default keyword

--- http/utils/parsers/param.parser.ts
+++ http/utils/parsers/param.parser.ts
@@ -4,10 +4,7 @@
  * @param {string} route
  * @returns {Map<string, string>}
  */
-export default function parseParam(
-  uri: string,
-  route: string,
-): Map<string, string> {
+export function parseParam(uri: string, route: string): Map<string, string> {
   const uriSegments: string[] = uri.split('/').slice(1);
   const routeSegments: string[] = route.split('/').slice(1);
 
